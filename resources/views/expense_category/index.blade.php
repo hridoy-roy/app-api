107825ca0cca11b4479a0828ad351a14c023bb32
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-03-24 16:13:56 +0600

    some minor update

--- resources/views/expense_category/index.blade.php
+++ resources/views/expense_category/index.blade.php
@@ -1,22 +1,24 @@
 @extends('layout.main')
 
 @section('content')
-<div>
-    <a class="btn btn-success" href="{{route('expense_categories.create')}}">Add New</a>
-</div>
+    <div>
+        <a class="btn btn-success" href="{{route('expense_categories.create')}}">Add New</a>
+    </div>
     <table class="table">
         <thead>
         <tr>
             <th scope="col">#</th>
             <th scope="col">Title</th>
+            <th scope="col">Total</th>
             <th scope="col">Action</th>
         </tr>
         </thead>
         <tbody>
         @forelse($allData as $data)
             <tr>
-                <th scope="row" >{{++$loop->index}}</th>
+                <th scope="row">{{++$loop->index}}</th>
                 <td>{{$data->title}}</td>
+                <td>{{$data->records->sum('amount')??0}}</td>
                 <td>
                     <a href="{{route('expense_categories.edit',$data->id)}}" class="btn btn-primary">Edit</a>
                     <form action="{{route('expense_categories.destroy',[$data->id])}}" method="POST">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,44 +2,50 @@
 
 @section('content')
 
-
-    <h1>Total Amount <span class="badge bg-secondary">{{$sum}}</span></h1>
-    <h2>Top Two Expense</h2>
-    @if(isset($data1['name']))
-    <h3>{{$data1['name']}} <span class="badge bg-secondary">{{$data1['amount']}} - ({{number_format(($data1['amount']/$sum)*100,2)}}%)</span></h3>
-    @endif
-    @if(isset($data2['name']))
-        <h3>{{$data2['name']}} <span class="badge bg-secondary">{{$data2['amount']}} - ({{number_format(($data2['amount']/$sum)*100,2)}}%)</span></h3>
-    @endif
-
-
-    <div class="m-3">
-        <a class="btn btn-success" href="{{route('expense_records.index')}}">Expense list </a>
+    <div class="text-center text-danger">
+        <h1>Total Amount </h1>
+        <h1>
+            <span class="badge bg-secondary">{{$sum}}</span>
+        </h1>
+    </div>
+    <div class="text-center">
+        <h2 class="text-success">Top Two Expense</h2>
+        @if(isset($data1['name']))
+            <h3><small>Category 1:</small> {{$data1['name']}} <span class="badge bg-secondary">{{$data1['amount']}} - ({{number_format(($data1['amount']/$sum)*100,2)}}%)</span>
+            </h3>
+        @endif
+        @if(isset($data2['name']))
+            <h3><small>Category 2:</small> {{$data2['name']}} <span class="badge bg-secondary">{{$data2['amount']}} - ({{number_format(($data2['amount']/$sum)*100,2)}}%)</span>
+            </h3>
+        @endif
     </div>
 
 
-    <div class="container">
-        <h1>Category List</h1>
+    <div class="m-3">
+        <a class="btn btn-success btn-lg" href="{{route('expense_records.index')}}">Expense list </a>
+    </div>
+
+
+    <div class="container text-center">
+        <h1 class="text-primary">Category List</h1>
         <div class="row">
             @forelse($categories as $category)
-            <div class="col-md-2 border">
-                <h2>{{$category->title}}</h2>
-            </div>
+                <div class="col-md-2 border">
+                    <h2>{{$category->title}}</h2>
+                    <span class="badge bg-secondary">{{$category->records->sum('amount') ?? 0}}</span>
+                </div>
             @empty
                 <div class="col-md-2">
                     <h2>No data</h2>
                 </div>
             @endforelse
         </div>
-        <div class="row m-3 p-0">
-            <div class="col-md-2">
-                <a class="btn btn-success" href="{{route('expense_categories.create')}}">Add Expense Category</a>
-            </div>
+        <div class="mt-4">
+                <a class="btn btn-success" href="{{route('expense_categories.create')}}">Add Expense Category +</a>
         </div>
     </div>
     <div>
 
     </div>
-
 
 @endsection
